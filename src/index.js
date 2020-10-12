4ded8b773f859c3890cb67b49f6236be594d8696
keshprince82 <keshprince82@gmail.com>
2020-10-12 12:43:49 +0530

    bug fixed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("i am from feature/FB01");
+<html>
+<body>
+
+<h2>JavaScript Variables</h2>
+
+<p>In this example, x is defined as a variable.
+Then, x is assigned the value of 6:</p>
+
+<p id="demo"></p>
+
+<script>
+var x;
+x = 6;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
